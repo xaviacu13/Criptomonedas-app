2bc86aa25ef486d20d3f68aa8be5e8ff1bf5ab03
Xavi <xavi.acu.13@gmail.com>
2021-10-05 23:11:29 -0400

    more setting and add images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import styled from  '@emotion/styled';
+import image from './cripto1.png'
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
 
 function App() {
   return (
-   <h1>Welcome to this app</h1>
+   <Container>
+     <div>
+       <Image
+        src={image}
+        alt="Cripto image"
+       />
+
+     </div>
+     <div>
+
+     </div>
+   </Container>
   );
 }
 
