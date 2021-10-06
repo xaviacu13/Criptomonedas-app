d83157db8c61ad775b878fac653b6b5c0d274976
Xavi <xavi.acu.13@gmail.com>
2021-10-06 14:06:14 -0400

    add styled to useMoneda hook

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,25 @@
 import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.3rem;
+`;
 
 const useMoneda = (moneda, stateInicial, options) => {
 
@@ -6,8 +27,8 @@ const useMoneda = (moneda, stateInicial, options) => {
 
   const Selecionar = () => (
     <>
-      <label>{moneda}</label>
-      <select
+      <Label>{moneda}</Label>
+      <Select
         onChange={ e => refreshState(e.target.value)}
         value={state}
       >
@@ -15,7 +36,7 @@ const useMoneda = (moneda, stateInicial, options) => {
         {options.map(op => (
           <option key={op.cod} value={op.cod}>{op.name}</option>
         ))}
-      </select>
+      </Select>
     </>
 
   );
